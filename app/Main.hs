25286b9094da2f738da5d56f785c20fce5c979c1
Sriram <ss2d22@soton.ac.uk>
2025-03-05 21:38:12 +0000

    added logic to skip spaces, added comments

--- app/Main.hs
+++ app/Main.hs
@@ -3,14 +3,23 @@ module Main where
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
 
+{-
+ - Checks if  the passed in character matches one of the symbols allowed in Scheme identifiers
+-}
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
+{-
+ - takes an input string, get's rid of spaces and checks if teh first charcter matches
+-}
 readExpr :: String -> String
 readExpr input = case parse (spaces >> symbol) "soton" input of
   Left err -> "No match: " ++ show err
   Right val -> "Found value"
 
+{-
+ - skips spaces
+-}
 spaces :: Parser ()
 spaces = skipMany1 space
 
