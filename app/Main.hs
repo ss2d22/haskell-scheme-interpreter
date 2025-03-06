9a55fbd471826b852cb92e7c39fb40025ec5c692
sriram <ss2d22@soton.ac.uk>
2025-03-06 18:58:10 +0000

    renamed the parser for now and added a custom data type

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,25 @@ import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
 
 {-
+ - Data type for the parser to return
+ - Atom - String naming the Atom
+ - List - Stores a list of other Goku values
+ - DottedList - an improper list where all 
+ - the values except the last value and the 
+ - last value is stored as a seperate entry
+ - Number - numbers
+ - String - strings
+ - Bool - booleans
+ -}
+data GokuVal = Atom String
+             | List [GokuVal]
+             | DottedList [GokuVal] GokuVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+
+{-
  - Checks if  the passed in character matches one of the symbols allowed in Scheme identifiers
 -}
 symbol :: Parser Char
@@ -13,7 +32,7 @@ symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
  - takes an input string, get's rid of spaces and checks if teh first charcter matches
 -}
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "soton" input of
+readExpr input = case parse (spaces >> symbol) "Goku" input of
   Left err -> "No match: " ++ show err
   Right val -> "Found value"
 
